4c4c0029c496d10dc51b8f5035bf3029588df8e6
Andrew Giang <giang184@gmail.com>
2021-06-18 16:41:52 -0700

    add order summary on html

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,14 @@ function Order() {
   this.pizzaList = {};
   this.currentID = 0;
   this.total = 0;
-  this.deliveryCost = false;
+  this.pizzaCount = 0;
 }
 
 Order.prototype.addPizza = function (pizza) {
   pizza.id = this.assignPizzaID();
   this.pizzaList[pizza.id]=pizza;
   this.total += pizza.calculateCost();
+  this.pizzaCount ++;
 };
 
 Order.prototype.assignPizzaID = function () {
@@ -30,15 +31,16 @@ Order.prototype.deletePizza = function (id) {
   }
   this.total -= this.pizzaList[id].calculateCost();
   delete this.pizzaList[id];
+  this.pizzaCount --;
   return true;
 }
 
 // Business Logic for Pizza ---------
-function Pizza (size, crust, sauce) {
+function Pizza (size, crust, sauce, toppings) {
   this.size = size;
   this.crust = crust;
   this.sauce = sauce;
-  //this.toppings = toppings;
+  this.toppings = toppings;
 }
 
 Pizza.prototype.calculateCost = function () {
@@ -56,13 +58,13 @@ Pizza.prototype.calculateCost = function () {
   if(this.crust === 'deep' || this.crust === 'stuffed')
     cost += 2;
 
-    //additional charges for toppings
-  // if(this.toppings.length < 2)
-  //   cost++;
-  // else if(this.toppings.length < 3)
-  //   cost+=2;
-  // else
-  //   cost+=4;
+  //additional charges for toppings
+  if(this.toppings.length < 2)
+    cost++;
+  else if(this.toppings.length < 3)
+    cost+=2;
+  else
+    cost+=4;
 
   return cost;
 }
@@ -75,18 +77,23 @@ function displayOrderDetails (orderToDisplay) {
   let text = "";
   Object.keys(orderToDisplay.pizzaList).forEach (function (key) {
     const pizza = orderToDisplay.findPizza(key);
-    text += "<li id=" +pizza.id + ">" + pizza.size + "</li>";
+    text += "<li id=" +pizza.id + ">" + pizza.size + ", " + pizza.toppings.length + " toppings" +"</li>";
   });
   pizzaUL.html(text);
 }
 
 function showPizza (pizzaID) {
   const pizza = myOrder.findPizza(pizzaID);
+  pizzaText=pizza.toppings[0];
+  for(let i=1; i<pizza.toppings.length; i++)
+  {
+    pizzaText += ", " + pizza.toppings[i];
+  }
   $("#show-pizza").show();
   $(".size").html(pizza.size);
   $(".crust").html(pizza.crust);
   $(".sauce").html(pizza.sauce);
-  //$(".toppings").html(pizza.toppings);
+  $(".toppings").html(pizzaText);
   let buttons = $("#buttons");
   buttons.empty();
   buttons.append("<button class='deleteButton' id=" + pizza.id + ">Delete</button>");
@@ -110,11 +117,52 @@ $(document).ready(function () {
     const inputtedSize = $("#size").val();
     const inputtedCrust = $("#crust").val();
     const inputtedSauce = $("#sauce").val();
-    //const inputtedToppings = $("#toppings").val();
+    const inputtedToppings = [];
+    if($('#cheese').is(':checked'))
+      inputtedToppings.push("cheese");
+    if($('#pepperoni').is(':checked'))
+      inputtedToppings.push("pepperoni");
+    if($('#sausage').is(':checked'))
+      inputtedToppings.push("sausage");
+    if($('#mushroom').is(':checked'))
+      inputtedToppings.push("mushroom");
+    if($('#pineapple').is(':checked'))
+      inputtedToppings.push("pineapple");
 
-
-    const newPizza = new Pizza(inputtedSize, inputtedCrust, inputtedSauce);
+    const newPizza = new Pizza(inputtedSize, inputtedCrust, inputtedSauce, inputtedToppings);
     myOrder.addPizza(newPizza);
     displayOrderDetails(myOrder);
   });
+
+  $("button#completeOrder").click(function() {
+    $("#bottom").toggle();
+  });
+
+
+  $("form#order").submit(function (event) {
+    event.preventDefault();
+    const inputtedName = $("#name").val();
+    const inputtedAddress = $("#address").val();
+  
+    $(".name").html(inputtedName);
+    $(".address").html(inputtedAddress);
+    $(".order").html(myOrder.pizzaCount);
+    $(".subtotal").html(myOrder.total);
+
+    if($("#option").val() === "delivery")
+    {
+      myOrder.total += 5;
+      $(".total").text( myOrder.total + " ($5 added for delivery)");
+    }
+    else {
+      $(".total").html(myOrder.total);
+    }
+
+    $("#orderSummary").show();
+  });
+
+  $("button#startNew").click(function() {
+    $("#test").toggle();
+  });
 });
+
